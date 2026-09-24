01131d826b77f526434a24ba404f2a37e37d81f9
dakotahr <iamdakotahr@gmail.com>
2026-09-24 17:33:24 -0300

    Add files via upload

--- src/commands/general/inventario.py
+++ src/commands/general/inventario.py
@@ -1,33 +1,24 @@
 from highrise import User
 
 class Command:
-def **init**(self, bot):
-self.bot = bot
-self.name = "inventario"
-self.description = "Muestra la ropa que tiene el bot"
-self.permissions = []
-self.cooldown = 5
+    def __init__(self, bot):
+        self.bot = bot
+        self.name = "inventario"
+        self.description = "Muestra la ropa que tiene el bot"
+        self.permissions = []
+        self.cooldown = 5
 
-```
-async def execute(self, user: User, args: list, message: str):
-    try:
-        inventario = await self.bot.highrise.get_inventory()
+    async def execute(self, user: User, args: list, message: str):
+        try:
+            inventario = await self.bot.highrise.get_inventory()
 
-        await self.bot.highrise.send_whisper(
-            user.id,
-            f"Respuesta de inventario: {inventario}"
-        )
+            await self.bot.highrise.send_whisper(
+                user.id,
+                f"Respuesta de inventario: {inventario}"
+            )
 
-        outfit = await self.bot.highrise.get_outfit()
-
-        await self.bot.highrise.send_whisper(
-            user.id,
-            f"Respuesta de outfit: {outfit}"
-        )
-
-    except Exception as e:
-        await self.bot.highrise.send_whisper(
-            user.id,
-            f"❌ Error al consultar:\n{type(e).__name__}: {e}"
-        )
-```
+        except Exception as e:
+            await self.bot.highrise.send_whisper(
+                user.id,
+                f"❌ Error al consultar inventario:\n{type(e).__name__}: {e}"
+            )
